4431319ea9a183a058d37ce7b55c9765180ef01c
ZackyExtreme <47761316+ZackyExtreme@users.noreply.github.com>
2019-03-11 20:09:19 -0300

    Add files via upload

---
+++ Cachorro.java
@@ -0,0 +1,24 @@
+
+public class Cachorro {
+	public String nome;
+	public int peso;
+	public String corOlhos;
+	public int quantPatas;
+	
+	public void falar() {
+		//Ação FALAR
+	}
+	
+	public void andar() {
+		//ação ANDAR
+	}
+	
+	public void comer() {
+		//ação COMER
+	}
+	
+	public void dormir() {
+		//ação DORMIR
+	}
+
+}
